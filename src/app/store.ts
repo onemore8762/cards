6e65b39e709ce80d375885e040e6859d7f3c2d1a
MaksDubina <mgd8832@gmail.com>
2022-12-10 17:33:55 +0700

    refactor: refactor store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,10 @@
-import { combineReducers, createStore } from 'redux'
-import { reducer } from './reducers'
+import { configureStore } from '@reduxjs/toolkit'
 
-const rootReducers = combineReducers({
-  reducer: reducer,
+export const store = configureStore({
+  reducer: {},
 })
 
-type AppStateType = ReturnType<typeof rootReducers>
-
-export const store = createStore(rootReducers)
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch
---
+++ src/common/hooks/react-redux-hooks.ts
@@ -0,0 +1,7 @@
+// import { AppRootStateType, AppThunkDispatch } from '../../app/store'
+// import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
+//
+// export const useAppDispatch = () => useDispatch<AppThunkDispatch>()
+//
+// export const useAppSelector: TypedUseSelectorHook<AppRootStateType> = useSelector
+export const chort = {}
