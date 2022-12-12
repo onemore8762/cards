086f1a39c03f4548a0a1e903fd23ed160bd2a1e5
onemore8762 <denis.churkin.afikei@mail.ru>
2022-12-12 23:01:08 +0300

    feat: add layout Registration.tsx

--- src/features/Registration/Registration.tsx
+++ src/features/Registration/Registration.tsx
@@ -1,5 +1,165 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import { Visibility, VisibilityOff } from '@mui/icons-material'
+import {
+  Button,
+  Card,
+  FormGroup,
+  FormLabel,
+  Grid,
+  IconButton,
+  InputAdornment,
+  TextField,
+} from '@mui/material'
+import { useFormik } from 'formik'
+import { NavLink } from 'react-router-dom'
 
 export const Registration = () => {
-  return <div>Registration</div>
+  const [showPassword, setShowPassword] = useState<any>({
+    password: false,
+    confirmPassword: false,
+  })
+
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+      confirmPassword: '',
+    },
+    validate: values => {
+      if (!values.email) {
+        return {
+          email: 'Email is required',
+        }
+      }
+      if (!values.password) {
+        return {
+          password: 'Password is required',
+        }
+      }
+    },
+    onSubmit: (values, { setErrors }) => {
+      if (values.password !== values.confirmPassword) {
+        setErrors({
+          password: "is don't match",
+          confirmPassword: "is don't match",
+        })
+      } else {
+        let { confirmPassword, ...data } = values
+
+        console.log(data)
+      }
+    },
+  })
+
+  const handleClickShowPassword = (value: string) => {
+    if (value === 'password') {
+      setShowPassword({ ...showPassword, password: !showPassword.password })
+    } else if (value === 'confirmPassword') {
+      setShowPassword({ ...showPassword, confirmPassword: !showPassword.confirmPassword })
+    }
+  }
+
+  return (
+    <Grid container justifyContent="center">
+      <Grid display="flex" justifyContent="center" alignItems="center">
+        <Card sx={{ width: '413px', height: '528px' }}>
+          <form onSubmit={formik.handleSubmit} style={{ padding: '33px 35px 0' }}>
+            <FormLabel sx={{ display: 'flex', justifyContent: 'center' }}>
+              <span style={{ color: '#000', fontSize: '26px', fontWeight: '600' }}>Sign Up</span>
+            </FormLabel>
+            <FormGroup sx={{ marginBottom: '68px' }}>
+              <TextField
+                fullWidth
+                id="email"
+                name="email"
+                label={'Email'}
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                error={formik.touched.email && Boolean(formik.errors.email)}
+                variant="standard"
+                margin={'normal'}
+                helperText={formik.touched.email && formik.errors.email}
+              />
+              <TextField
+                fullWidth
+                id="password"
+                name="password"
+                label="Password"
+                type={showPassword.password ? 'text' : 'password'}
+                value={formik.values.password}
+                onChange={formik.handleChange}
+                error={formik.touched.password && Boolean(formik.errors.password)}
+                variant="standard"
+                margin={'normal'}
+                helperText={formik.touched.password && formik.errors.password}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={() => handleClickShowPassword('password')}
+                      >
+                        {showPassword.password ? (
+                          <VisibilityOff sx={{ color: 'black' }} />
+                        ) : (
+                          <Visibility sx={{ color: 'black' }} />
+                        )}
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                }}
+              />
+              <TextField
+                fullWidth
+                id="confirmPassword"
+                name="confirmPassword"
+                label="Confirm password"
+                type={showPassword.confirmPassword ? 'text' : 'password'}
+                value={formik.values.confirmPassword}
+                onChange={formik.handleChange}
+                error={formik.touched.confirmPassword && Boolean(formik.errors.confirmPassword)}
+                variant="standard"
+                margin={'normal'}
+                helperText={formik.touched.confirmPassword && formik.errors.confirmPassword}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle confirmPassword visibility"
+                        onClick={() => handleClickShowPassword('confirmPassword')}
+                      >
+                        {showPassword.confirmPassword ? (
+                          <VisibilityOff sx={{ color: 'black' }} />
+                        ) : (
+                          <Visibility sx={{ color: 'black' }} />
+                        )}
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                }}
+              />
+            </FormGroup>
+            <Button variant="contained" sx={{ borderRadius: '30px', width: '347px' }} type="submit">
+              Sign Up
+            </Button>
+            <FormLabel
+              sx={{
+                display: 'flex',
+                justifyContent: 'center',
+                flexDirection: 'column',
+                alignItems: 'center',
+                mt: 2,
+              }}
+            >
+              <p style={{ fontWeight: '600', opacity: '50%' }}>Already have an account?</p>
+              <NavLink to={'/Login'} style={{ color: '#366EFF', fontWeight: '600' }}>
+                Sign In
+              </NavLink>
+            </FormLabel>
+          </form>
+        </Card>
+      </Grid>
+    </Grid>
+  )
 }
