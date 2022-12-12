e0a1054894cf5c611662237e6870cb8afd1f7a64
Crash2022 <crash555@mail.ru>
2022-12-13 00:20:38 +0500

    change profile styles

--- src/features/Profile/Profile.tsx
+++ src/features/Profile/Profile.tsx
@@ -2,14 +2,14 @@ import React, { useCallback } from 'react'
 
 // import Box from '@mui/material/Box'
 import AddAPhoto from '@mui/icons-material/AddAPhoto'
-import BorderColorOutlined from '@mui/icons-material/BorderColorOutlined'
+// import BorderColorOutlined from '@mui/icons-material/BorderColorOutlined'
 import ExitToAppOutlined from '@mui/icons-material/ExitToAppOutlined'
 import KeyboardBackspace from '@mui/icons-material/KeyboardBackspace'
 import Button from '@mui/material/Button'
 // import Button from '@mui/material-next/Button'
 import Card from '@mui/material/Card'
 import CardContent from '@mui/material/CardContent'
-import Typography from '@mui/material/Typography'
+// import Typography from '@mui/material/Typography'
 import Grid from '@mui/material/Unstable_Grid2'
 
 import { EditableSpan } from '../../common/components/EditableSpan/EditableSpan'
@@ -23,6 +23,8 @@ import style from './Profile.module.css'
 // )
 
 export const Profile = () => {
+  // const userName = useAppSelector<string>(state => state.profile.username)
+  // const userEmail = useAppSelector<string>(state => state.profile.useremail)
   const changeTaskTitleHandler = useCallback(
     (/*newInputValue: string*/) => {
       //dispatch(updateTaskTC(props.todolistId, props.task.id, { title: newInputValue }))
@@ -39,10 +41,13 @@ export const Profile = () => {
     alert('load photo')
     // dispatch(loadPhotoAC())
   }
+  const backHandler = () => {
+    alert('back')
+  }
 
   return (
     <Grid container justifyContent={'center'} style={{ position: 'relative' }}>
-      <div className={style.backButton}>
+      <div className={style.backButton} onClick={backHandler}>
         <KeyboardBackspace />
         <div className={style.backButton_title}>Back to Packs List</div>
       </div>
