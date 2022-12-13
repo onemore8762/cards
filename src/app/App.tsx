9a8430abd30d369ecd96d7fcac966be5729322c6
onemore8762 <denis.churkin.afikei@mail.ru>
2022-12-13 18:41:35 +0300

    add: error404 page, layout

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,7 +8,6 @@ import { NavBar } from '../common/components/NavBar/NavBar'
 import { Error404 } from '../features/Error404/Error404'
 import { Login } from '../features/Login/Login'
 import { CreateNewPassword } from '../features/Password/CreateNewPassword/CreateNewPassword'
-import { PasswordNew } from '../features/Password/Password_new'
 import { CheckEmail } from '../features/Password/recoveryPassword/CheckEmail'
 import { PasswordRecovery } from '../features/Password/recoveryPassword/Password_recovery'
 import { Profile } from '../features/Profile/Profile'
@@ -27,7 +26,6 @@ export const App = () => {
       <NavLink to={'/Registration'}>Registration</NavLink>
       <NavLink to={'/Error404'}>Error404</NavLink>
       <NavLink to={'/CreateNewPassword'}>CreateNewPassword</NavLink>
-      <NavLink to={'/PasswordNew'}>PasswordNew</NavLink>
       <NavLink to={'/checkEmail'}>checkEmail</NavLink>
       <NavLink to={'/PasswordRecovery'}>PasswordRecovery</NavLink>
       <div>-------------</div>
@@ -36,7 +34,6 @@ export const App = () => {
         <Route path={'/Profile'} element={<Profile />} />
         <Route path={'/Registration'} element={<Registration />} />
         <Route path={'/Error404'} element={<Error404 />} />
-        <Route path={'/PasswordNew'} element={<PasswordNew />} />
         <Route path={'/checkEmail'} element={<CheckEmail />} />
         <Route path={'/CreateNewPassword'} element={<CreateNewPassword />} />
         <Route path={'/PasswordRecovery'} element={<PasswordRecovery />} />
--- src/features/Error404/Error404.tsx
+++ src/features/Error404/Error404.tsx
@@ -1,5 +1,36 @@
 import React from 'react'
 
+import { Box, Button, Grid } from '@mui/material'
+import { useNavigate } from 'react-router-dom'
+
+import image404 from '../../assets/images/404.svg'
+
+import s from './Error404.module.css'
+
 export const Error404 = () => {
-  return <div>Error404</div>
+  let navigate = useNavigate()
+
+  return (
+    <Grid container justifyContent={'center'} alignItems={'center'}>
+      <Grid display="flex" justifyContent="center" alignItems="center" gap={'62px'}>
+        <Box>
+          <Box sx={{ marginBottom: '36px' }}>
+            <span className={s.header}>Ooops!</span>
+            <p className={s.description}>Sorry! Page not found!</p>
+          </Box>
+          <Button
+            className={s.button}
+            variant="contained"
+            sx={{ borderRadius: '30px' }}
+            onClick={() => {
+              navigate('/profile')
+            }}
+          >
+            Back to home page
+          </Button>
+        </Box>
+        <img src={image404} alt={'image-error404'} />
+      </Grid>
+    </Grid>
+  )
 }
